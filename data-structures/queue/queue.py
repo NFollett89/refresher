5dc83d512e75eaf164900b2fb4235fbc19507e12
Nick Follett <nfollett@google.com>
2018-06-11 19:54:22 -0700

    V1 of queue, no tests yet

---
+++ data-structures/queue/queue.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+
+class NFQueue(object):
+    def __init__(self):
+        self.queue = []
+
+    # Informal string representation of the Queue
+    def __str__(self):
+        return "%s" % self.queue
+
+    # Iterator implementation
+    def __iter__(self):
+        for data in self.queue:
+            yield data
+
+    # Len special method
+    def __len__(self):
+        return self.length()
+
+    # Return length of queue
+    def length(self):
+        return len(self.queue)
+
+    # Add data to the back of the queue
+    def enqueue(self, data):
+        self.queue.append(data)
+
+    # Remove data from the front of the queue and return it
+    def dequeue(self):
+        if len(self.queue) == 0:
+            return None
+        data = self.queue[0]
+        self.queue = self.queue[1:]
+        return data
+
+    # Return data from the front of the queue
+    def peek(self):
+        return self.queue[0]
+
+    # Clear the queue
+    def clear(self):
+        self.queue = []
+
+    # Checks if queue contains data
+    def contains(self, data):
+        return data in self.queue
+
